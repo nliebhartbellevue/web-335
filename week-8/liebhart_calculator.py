4d255a2a5d8376ec28c08467b7ba1916058d2c52
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-12-08 19:04:48 -0600

    exercise 8.3

---
+++ week-8/liebhart_calculator.py
@@ -0,0 +1,27 @@
+# Title: Calculator
+# Author: Nathaniel Liebhart
+# Date: December 8, 2019
+# Description: Exercise 8.3 Python Calculator
+
+# define add function
+
+
+def add(a, b):
+    return a + b
+
+# define subtract function
+
+
+def subtract(a, b):
+    return a - b
+
+# define divide function
+
+
+def divide(a, b):
+    return a / b
+
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
--- week-8/liebhart_hello_world.py
+++ week-8/liebhart_hello_world.py
@@ -1,1 +1,5 @@
+# Title: Hello World
+# Author: Nathaniel Liebhart
+# Date: December 8, 2019
+# Description: Exercise 8.2 Python Hello World
 print("Hello, World.")
